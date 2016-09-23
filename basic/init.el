1aafcd9ff7581debfa471fa4c279cb3b731f9313
pdex <christopherkeating@gmail.com>
2016-09-23 14:38:22 -0700

    got org-init working

--- basic/init.el
+++ basic/init.el
@@ -1,6 +1,9 @@
 ;; Setup package.el
 (require 'package)
+;; since we call package-initialize, we don't need emacs startup.el to do the same
 (setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
   '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (add-to-list 'package-archives 
@@ -16,3 +19,10 @@
 (use-package org
   :ensure t
   :pin org)
+
+(org-babel-load-file (expand-file-name "org-init/emacs-cheatsheet.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "org-init/shutdown.org" user-emacs-directory))
+
+(let ((custom-file (expand-file-name "custom.el" user-emacs-directory)))
+  (if (file-exists-p custom-file)
+    (load custom-file)))
